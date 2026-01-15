c2d1d63640eb0ab2646352b237ae9f94cad1d238
Mark-Cyber0628 <szakalmark0628@gmail.com>
2026-01-15 07:10:47 -0500

    dealer reviews

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from datetime import datetime
+from .restapis import get_request, analyze_review_sentiments
+
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -89,17 +91,21 @@ def get_dealer_details(request, dealer_id):
         return JsonResponse({"status":400,"message":"Bad Request"})
 
 def get_dealer_reviews(request, dealer_id):
-    # if dealer id has been provided
-    if(dealer_id):
-        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+    if dealer_id:
+        endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
+
         for review_detail in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
-            print(response)
-            review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"status":200,"reviews":reviews})
+            response = analyze_review_sentiments(review_detail.get('review', ''))
+            # Ha a sentiment service elhasal, ne dőljön el az egész endpoint
+            if response and isinstance(response, dict) and 'sentiment' in response:
+                review_detail['sentiment'] = response['sentiment']
+            else:
+                review_detail['sentiment'] = "neutral"
+
+        return JsonResponse({"status": 200, "reviews": reviews})
     else:
-        return JsonResponse({"status":400,"message":"Bad Request"})
+        return JsonResponse({"status": 400, "message": "Bad Request"})
 
 
 def get_cars(request):
